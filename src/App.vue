3c48cbf9231fd3c09578243464057fee6f821f88
murphy <1243917465@qq.com>
2022-02-13 15:33:18 +0800

    路由切换后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@
 
 <script lang="ts">
 import { provide, ref } from "vue"
+import { router } from "./router"
 
 
 export default {
   name: 'App',
   setup() {
     const width = document.documentElement.clientWidth
-    
+
     const asideVisible = ref(width <= 500 ? false : true)
     // provide 可以提供一个变量给子组件使用
     provide('asideVisible', asideVisible) // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        asideVisible.value = false
+      }
+
+    })
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
+import { router } from './router';
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createWebHashHistory, createRouter} from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
 import './index.scss'
 
-const history = createWebHashHistory()
-const router = createRouter({
-    history: history,
-    routes: [
-        {path: '/', component: Home},
-        {path: '/doc', component: Doc}
-    ]
-})
+
 
 const app = createApp(App)
 app.use(router)
