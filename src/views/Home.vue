b071ac7fa0d5cf16442e6cfb0d8f5cf9dd38aa13
murphy <1243917465@qq.com>
2022-02-13 13:32:42 +0800

    Home 页面样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Topnav/>
+        <Topnav />
         <div class="banner">
             <h1>木UI</h1>
             <h2>自制 UI 框架</h2>
@@ -12,11 +12,33 @@
     </div>
 </template>
 <style lang="scss" scoped>
-
 </style>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
 export default {
-    components: {Topnav}
+    components: { Topnav }
 }
 </script>
+<style lang="scss" scoped>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions {
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
+</style> 
