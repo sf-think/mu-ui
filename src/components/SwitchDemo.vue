2bc3109ca267e88aee2415735899322167a9a602
murphy <1243917465@qq.com>
2022-02-13 19:37:49 +0800

    Switch 组件点击切换

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        Switch 的文档
+        <Switch/>
     </div>
 </template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+    components: {Switch}
+}
+</script>
