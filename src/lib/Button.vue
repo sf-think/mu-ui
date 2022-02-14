a8052cc16a3ae5a0d37d5eb66c5de5690ee9494f
murphy <1243917465@qq.com>
2022-02-14 22:11:40 +0800

    支持 loading

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
     <button class="mu-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="mu-loadingIndicator"></span>
         <slot />
     </button>
 </template>
@@ -22,6 +23,9 @@ export default defineComponent({
         }, disabled: {
             type: Boolean,
             default: false,
+        }, loading: {
+            type: Boolean,
+            default: false
         },
     },
     setup(props) {
@@ -170,6 +174,25 @@ $grey: grey;
             cursor: not-allowed;
             color: $grey;
         }
+    }
+    > .mu-loadingIndicator {
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        margin-right: 4px;
+        border-radius: 8px;
+        border-color: $blue $blue $blue transparent;
+        border-style: solid;
+        border-width: 2px;
+        animation: mu-spin 1s infinite linear;
+    }
+}
+@keyframes mu-spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
     }
 }
 </style>
