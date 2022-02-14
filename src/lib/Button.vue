b7c0a152ffb293ed2542f2a26b7da3fab7d8a7dd
murphy <1243917465@qq.com>
2022-02-14 21:39:46 +0800

    Button 组件支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,18 @@ export default defineComponent({
         size: {
             type: String,
             default: "normal"
-        }
+        }, level: {
+            type: String,
+            default: "normal",
+        },
     },
     setup(props) {
-        const { theme, size } = props
+        const { theme, size, level } = props
         const classes = computed(() => {
             return {
                 [`mu-theme-${theme}`]: theme,
                 [`mu-size-${size}`]: size,
+                [`mu-level-${level}`]: level,
             }
         })
         return { classes }
@@ -37,6 +41,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .mu-button {
     box-sizing: border-box;
     height: $h;
@@ -53,6 +58,8 @@ $radius: 4px;
     border-radius: $radius;
     // sass fade-out 函数，使颜色更加透明
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
+
     // 除第一个 mu-button 以外的 mu-button
     & + & {
         margin-left: 8px;
@@ -87,16 +94,61 @@ $radius: 4px;
             background: darken(white, 5%);
         }
     }
+    &.mu-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
+    }
+    &.mu-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
+    }
     &.mu-theme-button {
-        &.mu-size-big {
-            font-size: 24px;
-            height: 48px;
-            padding: 0 16px;
+        &.mu-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
         }
-        &.mu-size-small {
-            font-size: 12px;
-            height: 20px;
-            padding: 0 4px;
+        &.mu-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
+    }
+    &.mu-theme-link {
+        &.mu-level-danger {
+            color: $red;
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+    &.mu-theme-text {
+        &.mu-level-main {
+            color: $blue;
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+        &.mu-level-danger {
+            color: $red;
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
         }
     }
 }
